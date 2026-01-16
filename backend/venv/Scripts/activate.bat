f4aadd24d8f4de6d99ea54df6914591a935b3aa5
pratik0765 <pratikpawar7028@gmail.com>
2026-01-16 09:00:59 +0530

    second comiit

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\prati\Desktop\React js\myprojectBackend\venv
+set VIRTUAL_ENV=C:\Users\prati\Desktop\FarmEra\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
